305023860751b4caf6fd9ef6ffdff0d601028936
Bilorukavsky <bilorukavskij@mail.ru>
2023-09-18 21:12:10 +0300

    update

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	//_ "github.com/lib/pq"
+	//"gorm.io/gorm"
 )
 
 type Delivery struct {
+	gorm.Model
 	Name    string `json:"name"`
 	Phone   string `json:"phone"`
 	Zip     string `json:"zip"`
@@ -18,9 +19,11 @@ type Delivery struct {
 	Address string `json:"address"`
 	Region  string `json:"region"`
 	Email   string `json:"email"`
+	OrderID int    `json:"order_id"`
 }
 
 type Payment struct {
+	gorm.Model
 	Transaction  string `json:"transaction"`
 	RequestID    string `json:"request_id"`
 	Currency     string `json:"currency"`
@@ -31,9 +34,11 @@ type Payment struct {
 	DeliveryCost int    `json:"delivery_cost"`
 	GoodsTotal   int    `json:"goods_total"`
 	CustomFee    int    `json:"custom_fee"`
+	OrderID      int    `json:"order_id"`
 }
 
 type Item struct {
+	gorm.Model
 	ChrtID      int    `json:"chrt_id"`
 	TrackNumber string `json:"track_number"`
 	Price       int    `json:"price"`
@@ -45,46 +50,55 @@ type Item struct {
 	NmID        int    `json:"nm_id"`
 	Brand       string `json:"brand"`
 	Status      int    `json:"status"`
+	OrderID     int    `json:"order_id"`
 }
 
 type Order struct {
 	gorm.Model
-	OrderUID        string   `json:"order_uid"`
-	TrackNumber     string   `json:"track_number"`
-	Entry           string   `json:"entry"`
+	OrderUID        *string  `json:"order_uid"`
+	TrackNumber     *string  `json:"track_number"`
+	Entry           *string  `json:"entry"`
 	Delivery        Delivery `json:"delivery"`
 	Payment         Payment  `json:"payment"`
 	Items           []Item   `json:"items"`
-	Locale          string   `json:"locale"`
-	InternalSig     string   `json:"internal_signature"`
-	CustomerID      string   `json:"customer_id"`
-	DeliveryService string   `json:"delivery_service"`
-	ShardKey        string   `json:"shardkey"`
+	Locale          *string  `json:"locale"`
+	InternalSig     *string  `json:"internal_signature"`
+	CustomerID      *string  `json:"customer_id"`
+	DeliveryService *string  `json:"delivery_service"`
+	ShardKey        *string  `json:"shardkey"`
 	SMID            int      `json:"sm_id"`
-	DateCreated     string   `json:"date_created"`
-	OOFShard        string   `json:"oof_shard"`
+	DateCreated     *string  `json:"date_created"`
+	OOFShard        *string  `json:"oof_shard"`
 }
 
 func main() {
 	// Подключение к базе данных
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Eror loading .env file")
+		log.Fatal("Ошибка загрузки .env файла")
 	}
 
-	dsn := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_USER") + " dbname=" + os.Getenv("DB_NAME") + " password=" + os.Getenv("DB_PASSWORD") + " port=" + os.Getenv("DB_PORT") + " sslmode=disable"
-	// Откройте соединение с базой данных
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{DisableForeignKeyConstraintWhenMigrating: true})
+	initializeDB()
+	defer closeDB() // Закрыть соединение с базой данных
+	migrateDB()     // Создание таблиц
+
+	clusterID := os.Getenv("CLUSTER_ID")
+	clientID := os.Getenv("CLIENT_ID")
+	channelName := os.Getenv("CHANNEL_NAME")
+
+	nc, err := connectToNATSStreaming(clusterID, clientID)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Ошибка подключения к NATS Streaming: %v", err)
 	}
+	defer nc.Close()
 
-	sqlDB, err := db.DB() // Получить экземпляр *sql.DB
+	subscription, err := subscribeToChannel(nc, channelName)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Ошибка подписки на канал: %v", err)
 	}
-	defer sqlDB.Close() // Закрыть соединение с базой данных
+	defer subscription.Close()
 
-	// Создание таблицы
-	db.AutoMigrate(&Order{})
+	log.Printf("Подписка на канал '%s'...\n", channelName)
+
+	waitForInterrupt()
 }
