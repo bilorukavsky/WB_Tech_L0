dc066c5283f3925403ffee37fbbb4376661960d9
Bilorukavsky <bilorukavskij@mail.ru>
2023-09-19 20:42:33 +0300

    Добавление скрипта

---
+++ script/script.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/nats-io/stan.go"
+)
+
+type Message struct {
+	Text string `json:"text"`
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Ошибка загрузки .env файла")
+	}
+
+	clusterID := os.Getenv("CLUSTER_ID")     // Замените на ваш CLUSTER_ID
+	clientID := os.Getenv("CLIENT_ID")       // Замените на ваш PUBLISHER_ID
+	channelName := os.Getenv("CHANNEL_NAME") // Замените на ваш CHANNEL_NAME
+
+	// Подключение к NATS Streaming
+	nc, err := stan.Connect(clusterID, clientID)
+	if err != nil {
+		log.Fatalf("Ошибка подключения к NATS Streaming: %v", err)
+	}
+	defer nc.Close()
+
+	// Бесконечный цикл для публикации сообщений
+	for {
+		message := Message{Text: "Пример сообщения"} // Замените на свои данные
+
+		messageJSON, err := json.Marshal(message)
+		if err != nil {
+			log.Printf("Ошибка при маршалинге JSON: %v", err)
+			continue
+		}
+
+		err = nc.Publish(channelName, messageJSON)
+		if err != nil {
+			log.Printf("Ошибка при публикации сообщения: %v", err)
+		} else {
+			fmt.Println("Сообщение успешно опубликовано.")
+		}
+
+		// Подождать некоторое время перед отправкой следующего сообщения
+		time.Sleep(5 * time.Second)
+	}
+}
